b9782e0f85f87c79570bdd29eb8d14426d0df107
Minhly <minh.y.ly@hotmail.com>
2020-10-15 13:12:09 +0200

    Game 1.0 Launch

---
+++ First OOP Game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace First_OOP_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Name Health Atk Block
+            Warrior maximus = new Warrior("Maximus", 1000, 200, 20);
+            Warrior bob = new Warrior("Bob", 1000, 200, 20);
+
+            Battle.StartFight(maximus, bob);
+
+            Console.ReadLine();
+        }
+    }
+}
